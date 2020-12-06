4e84c68774b066b84c32f76f0027fae7dacc2895
Dmitry Chepurovskiy <dm3ch@dm3ch.net>
2020-12-06 14:15:36 +0300

    Rewrite to actix and pg

--- migrations/2020-11-29-202617_create_reports_table/up.sql
+++ migrations/2020-11-29-202617_create_reports_table/up.sql
@@ -1,9 +1,10 @@
 CREATE TABLE reports (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    uiid UUID NOT NULL PRIMARY KEY,
     description VARCHAR NOT NULL,
     lat FLOAT NOT NULL ,
     lng FLOAT NOT NULL ,
-    completed BOOLEAN NOT NULL DEFAULT 0
+    status VARCHAR NOT NULL DEFAULT 'new'
 );
-INSERT INTO reports (description, lat, lng) VALUES ('Test: Volhov swamp is in fire', 59.92079346407353, 32.31912547869565);
-INSERT INTO reports (description, lat, lng) VALUES ('Test: Some Ladoga lake island is in fire.', 61.57500168623652, 31.371202935182904);
+
+INSERT INTO reports (uiid, description, lat, lng) VALUES ('86e3aed3-1553-5d23-8d61-2286215e65f1', 'Test: Volhov swamp is in fire', 59.92079346407353, 32.31912547869565);
+INSERT INTO reports (uiid, description, lat, lng) VALUES ('6eabff02-c968-5cbc-bc7f-3b672928a761', 'Test: Some Ladoga lake island is in fire.', 61.57500168623652, 31.371202935182904);
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,62 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder, middleware::Logger};
+#[macro_use]
+extern crate diesel;
 
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+use actix_web::{get, post, web, App, Error, HttpResponse, HttpServer, Responder, middleware::Logger};
+use diesel::prelude::*;
+use diesel::r2d2::{self, ConnectionManager};
+
+mod report;
+
+type DbPool = r2d2::Pool<ConnectionManager<PgConnection>>;
+
+#[post("/api/report")]
+async fn add_report(    
+    pool: web::Data<DbPool>,
+    form: web::Json<report::InsertableReport>,
+) -> Result<HttpResponse, Error> {
+    // let conn = pool.get().expect("couldn't get db connection from pool");
+
+    // use web::block to offload blocking Diesel code without blocking server thread
+    // let user = web::block(move || report::insert_report(&form.name, &conn))
+    //     .await
+    //     .map_err(|e| {
+    //         eprintln!("{}", e);
+    //         HttpResponse::InternalServerError().finish()
+    //     })?;
+    
+    println!("{:?}", form);
+    Ok(HttpResponse::Ok().body("Hello world!"))
 }
 
-#[get("/hello")]
-async fn hello1() -> impl Responder {
-    HttpResponse::Ok().body("Hello1 world!")
+#[get("/api/report")]
+async fn get_report() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
+    dotenv::dotenv().ok();
 
-    HttpServer::new(|| {
+    // set up database connection pool
+    let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL");
+    let manager = ConnectionManager::<PgConnection>::new(db_url);
+    let pool = r2d2::Pool::builder()
+        .build(manager)
+        .expect("Failed to create pool.");
+
+    let bind_addr = std::env::var("LISTEN_ADDR").expect("LISTEN_ADDR");
+    println!("Starting server at: {}", &bind_addr);
+
+    HttpServer::new(move || {
         App::new()
+            .data(pool.clone())
             .wrap(Logger::default())
-            .service(hello)
-            .service(hello1)
+            .service(add_report)
+            .service(get_report)
     })
-    .bind("127.0.0.1:8000")?
+    .bind(&bind_addr)?
     .run()
     .await
 }
